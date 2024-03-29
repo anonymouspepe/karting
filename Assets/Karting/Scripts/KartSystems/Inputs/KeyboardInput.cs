5cc2954487ca548be4343277175df1ace852de8a
anonymouspepe <anonymouspepe2024@gmail.com>
2024-03-29 13:59:17 +0800

    first commit

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems
+{
+
+    public class KeyboardInput : BaseInput
+    {
+        public string TurnInputName = "Horizontal";
+        public string AccelerateButtonName = "Accelerate";
+        public string BrakeButtonName = "Brake";
+
+        private bool accelerating = false;
+        private bool braking = false;
+        private float horizontal = 0.0f;
+
+        public override InputData GenerateInput()
+        {
+            return new InputData
+            {
+                Accelerate = accelerating,
+                Brake = braking,
+                TurnInput = horizontal
+            };
+        }
+
+        public void Accelerate()
+        {
+            accelerating = true;
+        }
+
+        public void DeAccelerate()
+        {
+            accelerating = false;
+        }
+
+        public void Brake()
+        {
+            braking = true;
+        }
+
+        public void DeBrake()
+        {
+            braking = false;
+        }
+
+        public void Left()
+        {
+            while (horizontal > -1.0f)
+            {
+                horizontal -= 0.05f;
+            }
+        }
+
+        public void Right()
+        {
+            while (horizontal < 1.0f)
+            {
+                horizontal += 0.05f;
+            }
+        }
+
+        public void DeTurn()
+        {
+            horizontal = 0.0f;
+        }
+    }
+}
